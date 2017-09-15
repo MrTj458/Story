b8c0d6566486f7ce7f4fbbde1b4d2772ec2d19fa
Trevor <mrtj458@gmail.com>
2017-09-14 23:41:09 -0600

    Added documentation to the Text class.

--- src/model/Text.java
+++ src/model/Text.java
@@ -1,3 +1,7 @@
+/**
+ * @author Trevor Hodsdon
+ */
+
 package model;
 
 import java.util.Scanner;
@@ -11,21 +15,37 @@ public class Text
 		scan = new Scanner(System.in);
 	}
 	
+	/**
+	 * Prints the inputted string to the console and adds a new line.
+	 * @param print
+	 */
 	public void println(String print)
 	{
 		System.out.println(print);
 	}
 	
+	/**
+	 * Prints the inputted string to the console.
+	 * @param print
+	 */
 	public void print(String print)
 	{
 		System.out.println(print);
 	}
 	
+	/**
+	 * Gets the next inputted value from the console.
+	 * @return Input from the console as a string.
+	 */
 	public String nextString()
 	{
 		return scan.next();
 	}
 	
+	/**
+	 * Gets the next inputted integer from the console.
+	 * @return The next integer inputted from the console.
+	 */
 	public int nextInt()
 	{
 		return scan.nextInt();
